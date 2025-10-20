8eeae6110ba68e2ee537734b8bd5d1a4eb599b47
medse <med.seffah@gmail.com>
2025-10-20 11:02:11 -0400

    add implementation for running migrations

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -10,6 +10,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from app.core.config import settings
 from app.core.db.database import Base
+from app.models import *
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
